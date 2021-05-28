065a2dc6530367f95a347be1f7e7879f00ceef33
justinge <43391856+justinge@users.noreply.github.com>
2021-05-28 23:14:06 +0800

    修改

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"go_upload/handler"
+	"net/http"
+)
 func main() {
-	
+	//注册路由
+	http.HandleFunc("/file/upload",handler.UploadHandler)
+	http.HandleFunc("/file/upload/suc",handler.UploadSucHandler)
+	err:=http.ListenAndServe(":8080",nil)
+	if err !=nil{
+		fmt.Printf("Failed to start server,err:%s",err.Error())
+	}
+
 }
