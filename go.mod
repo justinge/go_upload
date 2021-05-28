904a691da5183a2743647d9cf8e28e36d4b04ee7
justinge <43391856+justinge@users.noreply.github.com>
2021-05-28 22:44:55 +0800

    首次提交

---
+++ go.mod
@@ -0,0 +1,2 @@
+module go_upload
+go 1.14
---
+++ handler/handler.go
@@ -0,0 +1,14 @@
+package handler
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func UploadHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method
+}
+
